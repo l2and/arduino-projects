1a632c5b97a51fa94e504516c1ca413d5fb2f102
l2and <randall@rhidajat.com>
2022-01-17 12:40:49 -0800

    initialize first sketch
    setting up project to start work. Starting with only on-board LED

---
+++ 2d-leaf/2d-leaf.ino
@@ -0,0 +1,67 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example shows how to use Adafruit Feather M0 WiFi
+  to connect your project to Blynk.
+
+  Note: This requires WiFi101 library
+    from http://librarymanager/all#WiFi101
+
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+//Libraries
+#include "credentials.h"
+#include "config.h"
+
+#define BLYNK_TEMPLATE_ID "TMPLU_Zn7I7E"
+#define BLYNK_DEVICE_NAME "2dLeaf"
+
+BLYNK_WRITE(V0) // Executes when the value of virtual pin 0 changes
+{
+  if(param.asInt() == 1)
+  {
+    // execute this code if the switch widget is now ON
+    digitalWrite(13,HIGH);  // Set digital pin 2 HIGH
+  }
+  else
+  {
+    // execute this code if the switch widget is now OFF
+    digitalWrite(13,LOW);  // Set digital pin 2 LOW    
+  }
+}
+
+void setup()
+{
+    pinMode(13, OUTPUT); // Initialise digital pin 13 as an output pin
+
+  // Debug console
+  Serial.begin(9600);
+
+  WiFi.setPins(8, 7, 4, 2);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+}
---
+++ 2d-leaf/README.md
@@ -0,0 +1,13 @@
+# 2d-wall leaf
+Image of 2d-leaf
+
+## Description
+2d wall leaf used for backlighting powered by a wifi arduino, Blynk, and neopixels
+
+## Function
+
+
+## Arduino Parts Needed
+* [Adafruit Feather M0 Wifi](https://www.adafruit.com/product/3010)
+
+## Documentation Used
